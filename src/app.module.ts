0fd34b26eef89e4825533cc564a712c309709b8c
GHAOUI HAMDI <ghaoui.hamdi@gmail.com>
2021-12-05 19:53:37 +0100

    add simple user with mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import {MongooseModule} from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule,MongooseModule.forRoot('mongodb+srv://admin:admin@ghaoui.vuvds.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+import { User } from './user.model';
+
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  private users: User[];
+
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
+  async create(name: string, email: string, password: string) {
+    const newUser = new this.userModel({
+      name: name,
+      email: email,
+      password: password,
+    });
+    const result=  await newUser.save();
+    return result;
   }
 
   findAll() {
